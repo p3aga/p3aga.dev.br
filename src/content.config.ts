1827f0b4d4dfda2907ae1c249e748d7ac7d0e110
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-08-18 01:47:35 -0300

    feat: add collections blog & about

---
+++ src/content.config.ts
@@ -0,0 +1,30 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({
+    base: "./src/content/blog",
+    pattern: "**/*.{md,mdx}",
+  }),
+  schema: z.object({
+    title: z.string().max(30),
+    description: z.string().max(100),
+    pubDate: z.coerce.date(),
+    tags: z.array(z.string()).max(5),
+    draft: z.boolean().default(false),
+  }),
+});
+
+const about = defineCollection({
+  loader: glob({
+    base: "./src/content/about",
+    pattern: "**/*.{md,mdx}",
+  }),
+  schema: z.object({
+    title: z.string().max(30),
+    description: z.string().max(100),
+  }),
+});
+
+export const collections = { blog, about };
