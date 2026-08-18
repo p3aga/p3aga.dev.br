9f19d2c7db9f48a78e01bf3fad5c95e11e6272f1
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-08-17 22:35:43 -0300

    feat: add pagefind & astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +13,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [sitemap(), mdx()],
+  integrations: [pagefind(), sitemap(), mdx(), icon()],
 });
